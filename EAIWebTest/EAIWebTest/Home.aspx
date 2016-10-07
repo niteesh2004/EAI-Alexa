c0742501ad8b9a5536ed330a43f8752d9aeaa5b4
ANNAPC\nithishnaidu <niteesh2004@gmail.com>
2016-10-07 14:29:49 -0500

    Alexa top websites

---
+++ EAIWebTest/EAIWebTest/Home.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="EAIWebTest.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link href="bootstrap.css" rel="stylesheet" />
+
+    <link href="bootstrap.min.css" rel="stylesheet" />
+    
+    <!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h3 style="padding-left:450px"><i>List of Top 97 Domains </i></h3>
+    <div style="padding-top:40px">
+        
+        <asp:GridView ID="GridDomains" runat="server" HorizontalAlign="Center" HeaderStyle-BackColor="cadetblue" BorderColor="wheat"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ EAIWebTest/EAIWebTest/Home.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace EAIWebTest
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try {
+                string htmlCode = "";
+                int pagecount = 0;
+                string Weburl = ConfigurationManager.AppSettings["AlexaURL"].ToString();
+                List<string> domains = new List<string>();
+                string pattern = "\"/siteinfo/.*\"";
+                
+                while (domains.Count < 97)
+                {
+
+                    ///Downloading the Source code
+                    using (WebClient client = new WebClient())
+                    {
+
+                        htmlCode = client.DownloadString(Weburl);
+                    }
+
+
+
+                    ///Finding the Domain Names
+
+                    foreach (Match m in Regex.Matches(htmlCode, pattern, RegexOptions.IgnoreCase))
+                    {
+                        string[] getdomain = m.Value.Split('/');
+                        if (getdomain[2] != null)
+                        {
+                            getdomain[2] = getdomain[2].Trim('"');
+                            if (domains.Count < 97)
+                            {
+                                domains.Add(getdomain[2].ToString());
+                            }
+                            else {
+
+
+                                break;
+                            }
+                        }
+
+                    }
+
+                    ///Navigating to next page
+                    if (domains.Count < 97)
+                    {
+                        pagecount++;
+                        Weburl = ConfigurationManager.AppSettings["AlexaURL"].ToString() + "/global;" + pagecount + "";
+                    }
+                }
+
+                GridDomains.DataSource = ConvertListToDataTable(domains);
+                GridDomains.DataBind();
+                GridDomains.HeaderRow.Cells[0].Attributes["Width"] = "50px";
+               
+            }
+            catch( Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+            }
+
+        static DataTable ConvertListToDataTable(List<string> list)
+        {
+            int counter = 1;
+      
+            DataTable table = new DataTable();
+
+            table.Columns.Add("Rank");
+            table.Columns.Add("Domain Name");
+            
+
+           
+            foreach (var item in list)
+            {
+                table.Rows.Add(counter, item);
+                counter++;
+            }
+
+            return table;
+        }
+    }
+}
